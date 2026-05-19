49113af2cb95ebab1c8071dd18fbba8b59372465
Gockan <raipperssloverap@gmail.com>
2026-05-19 00:03:10 -0400

    Version final sistema gestion camaras

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,4 @@
-/*
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-});
-
-module.exports = pool;
-*/
-const { Pool } = require('pg');
+/*const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
@@ -20,6 +6,20 @@ const pool = new Pool({
     host: 'localhost',
     port: 5432,
     database: 'sistema_camaras'
+});
+
+module.exports = pool;*/
+const { Pool } = require('pg');
+
+const usarSSL = process.env.DB_HOST !== 'localhost';
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    ssl: usarSSL ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
